339d036eb6c62a0a3359c29c3133fc41c36ba71c
yu9824 <58211916+yu9824@users.noreply.github.com>
2025-08-16 15:46:09 +0900

    fix

--- api/index.ts
+++ api/index.ts
@@ -3,7 +3,6 @@ import { parseRequest } from './_lib/parser';
 import { getScreenshot } from './_lib/chromium';
 import { getHtml } from './_lib/template';
 
-const isDev = !process.env.VERCEL;
 const isHtmlDebug = process.env.OG_HTML_DEBUG === '1';
 
 export default async function handler(
